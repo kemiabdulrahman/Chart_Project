86123083242514698954b97d5183c67a95e0541c
kemiabdulrahman <106281283+kemiabdulrahman@users.noreply.github.com>
2024-10-27 17:00:35 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plot
 
-# Load data
+
 dataframe = pd.read_csv("jamb_exam_results.csv")
 
 
-# Plot 1: Histogram
+
 def plot_histogram(dataframe):
 
-    # Calculate statistical information
+    
     score_df = dataframe["JAMB_Score"]
     score_mean = score_df.mean()
     score_median = score_df.median()
@@ -49,10 +49,9 @@ def plot_histogram(dataframe):
     plot.close()
 
 
-# Plot 2: Scatter Plot
+
 def plot_scatter(dataframe):
 
-    # Calculate statistical information
     jamb_score_mean = dataframe["JAMB_Score"].mean()
     study_hours_per_week_mean = dataframe["Study_Hours_Per_Week"].mean()
     attendance_rate_mean = dataframe["Attendance_Rate"].mean()
@@ -67,7 +66,6 @@ def plot_scatter(dataframe):
     plot.xlabel("Jamb Score")
     plot.ylabel("Study Hours Per Week")
 
-    # Create a text box with statistics to place on the plot
     stats_info = (
         f"Jamb Score Mean: {jamb_score_mean:.2f}\n"
         f"Jamb Score Median: {jamb_score_median:.2f}\n"
@@ -75,7 +73,6 @@ def plot_scatter(dataframe):
         f"Attendance Rate Mean: {attendance_rate_mean:.2f}"
     )
 
-    # Position text box on the top right corner
     plot.gca().text(
         1.05,
         0.95,
@@ -87,8 +84,7 @@ def plot_scatter(dataframe):
         bbox=dict(facecolor="white", alpha=0.5),
     )
 
-    # Show plot
-    plot.tight_layout(pad=3)  # Add padding to prevent clipping
+    plot.tight_layout(pad=3)  
 
     plot.savefig("plots/scatter.png")
     plot.close()
@@ -103,7 +99,7 @@ def plot_heatmap(dataframe):
     sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
     plot.title("Heatmap of Correlation Matrix")
 
-    # Calculate high and low correlations for annotation
+    
     high_corr = [
         (x, y, correlation_matrix.loc[x, y])
         for x in correlation_matrix.columns
@@ -117,13 +113,11 @@ def plot_heatmap(dataframe):
         if (abs(correlation_matrix.loc[x, y]) < 0.2) and (x != y)
     ]
 
-    # Format annotation text
     high_corr_text = "\n".join([f"{x} & {y}: {corr:.2f}" for (x, y, corr) in high_corr])
     low_corr_text = "\n".join([f"{x} & {y}: {corr:.2f}" for (x, y, corr) in low_corr])
 
-    # Place high and low correlation texts on the far right side of the plot
     plot.gca().text(
-        1.25,  # Move the text further right
+        1.25,  
         0.8,
         f"High Correlations:\n{high_corr_text}",
         transform=plot.gca().transAxes,
@@ -143,12 +137,10 @@ def plot_heatmap(dataframe):
         bbox=dict(facecolor="white", alpha=0.5),
     )
 
-    # Save the figure with additional padding for the annotations
     plot.savefig("plots/heatmap.png", bbox_inches="tight")
     plot.close()
 
 
-# Run the functions
 plot_histogram(dataframe)
 plot_scatter(dataframe)
 plot_heatmap(dataframe)
